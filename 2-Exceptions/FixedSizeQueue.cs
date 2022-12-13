bcc3eaeb4828b105544f8343d2900eaefd8da25c
Riccardo Mazzi - riccardo.mazzi@studio.unibo.it <riccardo.mazzi@studio.unibo.it>
2022-12-13 16:58:09 +0100

    finito es2

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -19,7 +19,9 @@ namespace Exceptions
 
         public object GetFirst()
         {
-            // TODO ensure objects can only be retrieve if the item is queue is not empty
+            if (Count == 0) {
+                throw new EmptyQueueException();
+            }
             var first = _items[_firstIndex % Capacity];
             _firstIndex++;
             return first;
@@ -27,7 +29,9 @@ namespace Exceptions
         
         public void AddLast(object item)
         {
-            // TODO ensure objects can only be inserted if the item is queue is not full
+            if (Count == Capacity) {
+                throw new FullQueueException();
+            }
             _items[_lastIndex % Capacity] = item;
             _lastIndex++;
         }
